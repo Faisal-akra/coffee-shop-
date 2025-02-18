b9eddca4b05413bb6394a29a04c438789e802951
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-02-18 23:47:41 +0500

    about sec is responsive

--- src/myComponents/About.jsx
+++ src/myComponents/About.jsx
@@ -2,17 +2,17 @@ import cup from '../assets/about/cup.png'
 
 const About = () => {
   return(
-    <div className="flex justify-evenly  items-center mb-10 ">
+    <div className=" mt-5 lg:mt-0 flex justify-evenly  items-center mb-10">
 
-    <div className="flex flex-col justify-center items-center text-3xl gap-2">
-      <h1 className='font-extrabold text-amber-700 font-mono text-3xl' >Coffee with Faisal</h1>
-      <h2>Coffee is special in this shop</h2>
-      <button className='border bg-amber-700 rounded-sm text-white '>View-more</button>
+    <div className="  text-3xl  flex flex-col justify-center items-center  gap-2">
+      <h1 className='text-sm  sm:text-2xl md:text-2xl xlg:text-3xl  font-extrabold text-amber-700 font-mono ' >Coffee with Faisal</h1>
+      <h2 className='text-sm md:text-lg'>Coffee is special in this shop</h2>
+      <button className='text-sm sm:border bg-amber-700 rounded-sm text-white '>View-more</button>
     </div>
 
 
     <div className="">
-    <img src={cup} alt="cup" />
+    <img src={cup} alt="cup" className='w-32 sm:w-40 lg:w-72 xlg:w-[500px]' />
     </div>
 
 
