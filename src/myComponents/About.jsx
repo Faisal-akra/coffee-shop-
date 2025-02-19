b9577ac000c776343c4a9916551faf0191ea3e45
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-02-19 22:44:42 +0500

    coffes sec is responsive now

--- src/myComponents/About.jsx
+++ src/myComponents/About.jsx
@@ -1,25 +1,30 @@
-import cup from '../assets/about/cup.png'
+import cup from "../assets/about/cup.png";
 
 const About = () => {
-  return(
-    <div className=" mt-5 lg:mt-0 flex justify-evenly  items-center mb-10">
+  return (
+    <>
+      <div className=" mt-5 lg:mt-10 flex justify-evenly  items-center mb-16  ">
+        <div className="  text-3xl  flex flex-col justify-center items-center  gap-2">
+          <h1 className="text-sm  md:text-lg  lg:text-3xl   font-extrabold text-amber-700 font-mono ">
+            Coffee with Faisal
+          </h1>
+          <h2 className="text-sm md:text-lg lg:text-3xl lg:text-center">
+            Coffee is special in this shop
+          </h2>
+          <button className="text-sm md:text-lg lg:text-3xl   border bg-amber-700 rounded-sm text-white ">
+            View-more
+          </button>
+        </div>
 
-    <div className="  text-3xl  flex flex-col justify-center items-center  gap-2">
-      <h1 className='text-sm  sm:text-2xl md:text-2xl xlg:text-3xl  font-extrabold text-amber-700 font-mono ' >Coffee with Faisal</h1>
-      <h2 className='text-sm md:text-lg'>Coffee is special in this shop</h2>
-      <button className='text-sm sm:border bg-amber-700 rounded-sm text-white '>View-more</button>
-    </div>
-
-
-    <div className="">
-    <img src={cup} alt="cup" className='w-32 sm:w-40 lg:w-72 xlg:w-[500px]' />
-    </div>
-
-
-
-
-
-    </div>
-  )
-}
+        <div className="">
+          <img
+            src={cup}
+            alt="cup"
+            className="w-32 sm:w-40 lg:w-[60%] 2xl:w-[100%]"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 export default About;
--- src/myComponents/Coffees.jsx
+++ src/myComponents/Coffees.jsx
@@ -4,34 +4,34 @@ import cup from "../assets/Coffees/cup.png";
 export const Coffes = () => {
   return (
     <>
-    <div className="  p-5  grid grid-cols-3 grid-rows-2  justify-items-center gap-12 relative  bg-no-repeat bg-cover bg-center "
+    <div className=" grid grid-cols-2 gap-5  md:grid md:grid-cols-3 grid-rows-2  justify-items-center md:gap-12 relative  bg-no-repeat bg-cover bg-center p-5 "
     // style={{backgroundImage: `url(${drink})`, filter: 'opacity(0.5)' }}
 
     >
 
 
-      <div className=" border-black w-48 h-48  flex justify-center  relative  items-center rounded-lg border-r-5 border-2 ">
+      <div className=" w-32 h-32  md:w-48 md:h-48  flex justify-center  relative  items-center rounded-lg border-r-5 border-2 border-black ">
         <img src={cup} alt="" className="w-32 h-32   absolute top-[-65px] rounded-full"  />
       </div>
 
 
-      <div className=" border-black w-48 h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 ">
+      <div className=" w-32 h-32  md:w-48 md:h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 border-black">
         <img src={cup} alt="" className="w-32 h-32   absolute top-[-65px] rounded-full"  />
       </div>
 
-      <div className=" border-black w-48 h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 ">
+      <div className=" w-32 h-32  md:w-48 md:h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 border-black ">
         <img src={cup} alt="" className="w-32 h-32   absolute top-[-65px] rounded-full"  />
       </div>
 
-      <div className=" border-black w-48 h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 ">
+      <div className=" w-32 h-32 md:w-48 md:h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2  border-black">
         <img src={cup} alt="" className="w-32 h-32   absolute top-[-65px] rounded-full"  />
       </div>
 
-      <div className=" border-black w-48 h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 ">
+      <div className=" w-32 h-32  md:w-48 md:h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 border-black">
         <img src={cup} alt="" className="w-32 h-32   absolute top-[-65px] rounded-full"  />
       </div>
 
-      <div className=" border-black w-48 h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 ">
+      <div className=" w-32 h-32  md:w-48 md:h-48   flex justify-center  relative  items-center rounded-lg border-r-5 border-2 border-black">
         <img src={cup} alt="" className="w-32 h-32   absolute top-[-65px] rounded-full"  />
       </div>
 
