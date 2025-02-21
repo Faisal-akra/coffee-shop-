68f41a99435815cf075e5cb4c30bbfb55b8e9796
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-02-21 10:39:48 +0500

    beans and cafeteria sec is responsive now

--- src/myComponents/Beans.jsx
+++ src/myComponents/Beans.jsx
@@ -3,12 +3,12 @@ import packet from "../assets/Beans/packet.png";
 export const Beans = () => {
   return (
     <div>
-      <div className="grid grid-cols-2 justify-items-center gap-10 p-5   md:grid md:grid-cols-3 md:grid-rows-2 md:gap-14     md:justify-items-center lg:grid-cols-4   ">
+      <div className="grid grid-cols-2 justify-items-center gap-10 p-5   md:grid md:grid-cols-3 md:grid-rows-2  md:gap-14     lg:grid-cols-4    ">
 
 
         <div className="  w-40 h-40 group">
 
-          <div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden    md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black      border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+          <div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden    md:w-48 md:h-48 rounded-xl   md:flex md:flex-col   md:justify-center  border-black      border-2 md:overflow-hidden group-hover:bg-amber-700  ">
             <img
               src={packet}
               alt=""
--- src/myComponents/DrinkingSec.jsx
+++ src/myComponents/DrinkingSec.jsx
@@ -3,14 +3,18 @@ import drinnk2 from "../assets/drinking/drink2.png";
 
 export const Drinking = () => {
   return (
-    <div className="flex justify-around ">
-      <div className="w-[600px] h-[500px] border border-black" >
+    <div className="flex justify-around gap-4">
+
+
+      <div className="w-[300px] h-[200px]         md:w-[400px] md:h-[400px] xl:w-[600px] xl:h-[500px] border border-black rounded-2xl" >
         <img src={drink1} alt="" className="w-full h-full rounded-2xl" />
       </div>
 
-      <div className="w-[600px] h-[500px] border border-black">
+      <div className="w-[300px] h-[200px]         md:w-[400px] md:h-[400px] xl:w-[600px] xl:h-[500px] border border-black rounded-2xl">
         <img src={drinnk2} alt="" className="w-full h-full rounded-xl" />
       </div>
+
+
     </div>
   );
 };
