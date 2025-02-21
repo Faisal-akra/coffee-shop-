f78e62a138c809e1521b730c94cc18b0100cd971
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-02-21 08:49:17 +0500

    Beans sec is responsive now

--- src/myComponents/Beans.jsx
+++ src/myComponents/Beans.jsx
@@ -1,109 +1,159 @@
 import packet from "../assets/Beans/packet.png";
-// import bag from '../assets/Beans/check.png'
+
 export const Beans = () => {
   return (
     <div>
-      <div className="  p-5 grid grid-cols-4 gap-7 justify-items-center  ">
+      <div className="grid grid-cols-2 justify-items-center gap-10 p-5   md:grid md:grid-cols-3 md:grid-rows-2 md:gap-14     md:justify-items-center lg:grid-cols-4   ">
 
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
 
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
+        <div className="  w-40 h-40 group">
+
+          <div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden    md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black      border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+            <img
+              src={packet}
+              alt=""
+              className=" w-20 h-20 md:w-36 md:h-36 md:p-2 hover:rounded-xl  "
+            />
+
+            <div className=" flex  justify-center items-center p-2 md:flex-col  group-hover:text-white ">
               <h2>This beans capahino</h2>
-              <h3>price $30</h3>
+              <h3 className="md:mb-2">price $30</h3>
             </div>
-          </div>
-        </div>
 
-
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
-
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
-          </div>
-
-
-        </div> <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
-
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
           </div>
         </div>
 
 
 
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
+        <div className="  w-40 h-40 group">
 
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
-          </div>
-        </div>
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden    md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
 
+  <div className=" flex  justify-center items-center p-2 md:flex-col group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
 
-
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
-
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
-          </div>
-        </div>
+</div>
+</div>
 
 
+<div className="  w-40 h-40 group">
 
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden     md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
 
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
+  <div className=" flex  justify-center items-center p-2 md:flex-col group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
 
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
-          </div>
-        </div>
+</div>
+</div>
 
 
 
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
+<div className="  w-40 h-40 group">
 
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
-          </div>
-        </div>
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden     md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
+
+  <div className=" flex  justify-center items-center p-2 md:flex-col group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
+
+</div>
+</div>
 
 
 
-        <div className="w-52 h-56 group">
-          <div className=" border-black w-52 h-56 rounded-xl   flex flex-col  items-center  justify-center    border-2 overflow-hidden group-hover:bg-amber-700  ">
-            <img src={packet} alt="" className="w-40 h-40 hover:rounded-xl  " />
+<div className="  w-40 h-40 group">
 
-            <div className="flex flex-col justify-center items-center group-hover:text-white">
-              <h2>This beans capahino</h2>
-              <h3>price $30</h3>
-            </div>
-          </div>
-        </div>
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden     md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
+
+  <div className=" flex  justify-center items-center p-2 md:flex-col group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
+
+</div>
+</div>
+
+
+<div className="  w-40 h-40 group">
+
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden     md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
+
+  <div className=" flex  justify-center items-center p-2 md:flex-col  group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
+
+</div>
+</div>
+
+
+
+
+
+<div className="  w-40 h-40 group">
+
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden     md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
+
+  <div className=" flex  justify-center items-center p-2 md:flex-col group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
+
+</div>
+</div>
+
+
+<div className="  w-40 h-40 group">
+
+<div className="w-36 h-36  justify-center  flex flex-col items-center  overflow-hidden     md:w-48 md:h-48 rounded-xl   md:flex md:flex-col  md:items-center  md:justify-center  border-black     border-2 md:overflow-hidden group-hover:bg-amber-700  ">
+  <img
+    src={packet}
+    alt=""
+    className=" w-20 h-20 md:w-36 md:h-36 hover:rounded-xl  "
+  />
+
+  <div className=" flex  justify-center items-center p-2 md:flex-col group-hover:text-white ">
+    <h2>This beans capahino</h2>
+    <h3 className="md:mb-2">price $30</h3>
+  </div>
+
+</div>
+</div>
 
       </div>
     </div>
